6782c4dd95b98d5890dfbe88678265085fb3ce51
Josie Chen <josieyxchen@gmail.com>
2020-12-01 14:47:48 -0500

    feat: successfully set up auth

--- App.js
+++ App.js
@@ -2,6 +2,11 @@ import React, { useState } from 'react';
 import { View, Alert, Text, TouchableOpacity, Image } from 'react-native';
 import ImagePicker from 'react-native-image-picker';
 
+import Amplify from 'aws-amplify'
+import config from './aws-exports'
+Amplify.configure(config)
+import { withAuthenticator } from 'aws-amplify-react-native'
+
 const App = () => {
   const [image, setImage] = useState(null);
 
@@ -46,4 +51,4 @@ const App = () => {
 }
 
 
-export default App;
+export default withAuthenticator(App);
