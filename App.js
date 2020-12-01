355d929ec6a83bc28b4066e1abd190f4f7d0beb5
Josie Chen <josieyxchen@gmail.com>
2020-11-30 22:28:10 -0500

    feat: app can take a photo or pick a photo from library and show it

--- App.js
+++ App.js
@@ -1,21 +1,55 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { View, Alert, Text, TouchableOpacity, Image } from 'react-native';
+import ImagePicker from 'react-native-image-picker';
 
-export default function App() {
+console.log("what is this", ImagePicker)
+
+const App = () => {
+  const [image, setImage] = useState(null);
+
+  function selectImage() {
+    let options = {
+      title: 'You can choose one image',
+      maxWidth: 256,
+      maxHeight: 256,
+      storageOptions: {
+        skipBackup: true
+      }
+    };
+
+    ImagePicker.showImagePicker(options, response => {
+      console.log({ response });
+
+      if (response.didCancel) {
+        console.log('User cancelled photo picker');
+        Alert.alert('You did not select any image');
+      } else if (response.error) {
+        console.log('ImagePicker Error: ', response.error);
+      } else if (response.customButton) {
+        console.log('User tapped custom button: ', response.customButton);
+      } else {
+        let source = { uri: response.uri };
+        setImage(response.uri)
+      }
+    });
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>
+        Simple Image Picker
+      </Text>
+      <TouchableOpacity
+        onPress={selectImage}
+      >
+        <Text>Pick an image</Text>
+      </TouchableOpacity>
+      {image && (
+        <Image source={{ uri: image }} style={{ width: 200, height: 200 }} />
+      )}
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
+export default App;
