c94d3c52b277c405fa0bef9414e6838e56707e51
Christian Fernandez <tdc027@protonmail.com>
2020-12-03 19:30:18 -0800

    Basic heatmap with dummy data done. Need to have a hosted website that has all the geojson data printed on there in order for mapbox to work. The hosted website needs the most updated version of dynamo (get)

--- Map.js
+++ Map.js
@@ -18,19 +18,60 @@ const styles = StyleSheet.create({
   },
   map: {
     flex: 1
-  }
+  },
 });
 
 export default class App extends Component {
   componentDidMount() {
-    MapboxGL.setTelemetryEnabled(false);
+    MapboxGL.setTelemetryEnabled(true);
   }
 
+  //GET response object from Dynamo
+  //Go through each object's key's and extract out the lat/lng
+  //Store those pairs in a geoJSON format
+  //STore that geoJSON to some AWS service
+  //Retriece that data via URL
+  //The data needs to
+
+
+  //Upon rendering,
   render() {
     return (
       <View style={styles.page}>
         <View style={styles.container}>
-          <MapboxGL.MapView style={styles.map} />
+          <MapboxGL.MapView style={styles.map}>
+            <MapboxGL.Camera
+              zoomLevel={20}
+              centerCoordinate={[-74.009499, 40.704498]}
+            />
+            <MapboxGL.ShapeSource
+              id="earthquakes"
+              url="https://www.mapbox.com/mapbox-gl-js/assets/earthquakes.geojson">
+              <MapboxGL.HeatmapLayer
+                id="earthquakes"
+                sourceID="earthquakes"
+                style={{
+                  heatmapColor: [
+                    'interpolate',
+                    ['linear'],
+                    ['heatmap-density'],
+                    0,
+                    'rgba(33,102,172,0)',
+                    0.2,
+                    'rgb(103,169,207)',
+                    0.4,
+                    'rgb(209,229,240)',
+                    0.6,
+                    'rgb(253,219,199)',
+                    0.8,
+                    'rgb(239,138,98)',
+                    1,
+                    'rgb(178,24,43)',
+                  ],
+                }}
+              />
+            </MapboxGL.ShapeSource>
+          </MapboxGL.MapView>
         </View>
       </View>
     );
