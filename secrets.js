9871f59d29b17d1ed1e5d6bea41fe044427dd160
Romina Ionascu <romina.draghici@gmail.com>
2020-12-04 15:11:54 -0600

    fixed branch for match

--- secrets.js
+++ secrets.js
@@ -1,17 +1,4 @@
-process.env.AWS_ACCESS_KEY_ID = "AKIAYXCXQMBACR6H7NVC";
-process.env.AWS_SECRET_ACCCESS_KEY = "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";
-
-// const uploadToStorage = async (pathToImageFile, title) => {
-//   try {
-//     const response = await fetch(pathToImageFile);
-//     const blob = await response.blob();
-//     Storage.put(title, blob, {
-//       contentType: 'image/jpeg',
-//     });
-//   } catch (err) {
-//     console.log(err);
-//   }
-// };
-
-// 16:48
-// import Amplify, { Storage } from 'aws-amplify'
+// process.env.AWS_ACCESS_KEY_ID = "AKIAYXCXQMBACR6H7NVC";
+process.env.AWS_ACCESS_KEY_ID = "AKIAYXCXQMBADGA5BLFH";
+// process.env.AWS_SECRET_ACCCESS_KEY = "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";
+process.env.AWS_SECRET_ACCCESS_KEY = "ySJyPBrSJ06T+At9iWxvriIMiSufYED8i8ggLMQi";
