fdf6d6dcc60756912b7ed0ffff25a136f4068f39
Romina Ionascu <romina.draghici@gmail.com>
2020-12-06 10:56:01 -0600

    display info of user if is a match

--- ScanStack.js
+++ ScanStack.js
@@ -1,22 +1,17 @@
-import * as React from 'react';
-import { createStackNavigator } from '@react-navigation/stack';
-import Scan from "./Scan"
-import Profile from "./Profile"
+import * as React from "react";
+import { createStackNavigator } from "@react-navigation/stack";
+import Scan from "./Scan";
+import Profile from "./Profile";
 
 const Stack = createStackNavigator();
 
 const ScanStack = () => {
   return (
-    // <NavigationContainer >
-      <Stack.Navigator>
-        <Stack.Screen
-          name="Scan"
-          component={Scan}
-        />
-        <Stack.Screen name="Profile" component={Profile} />
-      </Stack.Navigator>
-    // </NavigationContainer>
-  )
-}
+    <Stack.Navigator>
+      <Stack.Screen name="Scan" component={Scan} />
+      <Stack.Screen name="Profile" component={Profile} />
+    </Stack.Navigator>
+  );
+};
 
 export default ScanStack;
